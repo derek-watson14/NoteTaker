c1146fe262ba830668933bddddd4955341a90c43
Derek Watson <derek.watson92@gmail.com>
2020-07-13 23:51:57 -0700

    addded / route

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + "/public", "index.html"))
 });
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public", "index.html"))
+});
+
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname + "/public", "notes.html"))
 });
