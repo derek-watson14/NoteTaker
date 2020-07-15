80bed8862ac7742bdf67c72537fad4bd41ccfa7d
Derek Watson <derek.watson92@gmail.com>
2020-07-14 23:23:26 -0700

    completed all routes, fully functioning

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -11,50 +12,62 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+// Helpers
+function randomId() {
+  return "_" + Math.random().toString(36).substr(2, 9);
+}
 
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public", "index.html"))
-});
-
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public", "index.html"))
+  res.sendFile(path.join(__dirname + "/public", "index.html"));
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public", "notes.html"))
+  res.sendFile(path.join(__dirname + "/public", "notes.html"));
 });
 
-// Create New reservation - takes in JSON input
-app.post("/api/tables", function (req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newReservation = req.body;
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    return res.json(JSON.parse(data));
+  });
+});
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+app.post("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    req.body.id = randomId();
+    notes.push(req.body);
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      else res.json({ msg: "Success" });
+    });
+  });
+});
 
-  // newReservation.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+app.delete("/api/notes/:id", function (req, res) {
+  var deleteId = req.params.id;
+  console.log(deleteId);
+  fs.readFile(__dirname + "/db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    var updatedNotes = notes.filter(({ id }) => id !== deleteId);
+    var stringNotes = JSON.stringify(updatedNotes);
 
-  console.log(newReservation);
-
-  if (tables.length < 5) {
-    tables.push(newReservation);
-    res.send("Table")
-  } else {
-    waitingList.push(newReservation);
-    res.send("Waitlist")
-  }
-
-
+    fs.writeFile(__dirname + "/db/db.json", stringNotes, (err) => {
+      if (err) throw err;
+      else res.json({ msg: "Success" });
+    });
+  });
 });
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on http://localhost:" + PORT);
 });
